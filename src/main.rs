4c8f09b627924208e5f0abbaeea25fda051a2049
Daniil Rybalko <pslpslpslpsl@yandex.ru>
2022-11-24 01:14:25 +0300

    working (x, y, h) location, parallel, fast

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ static ALBEDO: &'static [f64] =
 static SIZE: usize = 200;
 static LIGHT_LUMINOSITY: f64 = 1.0;
 static DIAG: f64 = (SIZE*3 * SIZE * 3 + SIZE*3 * SIZE*3) as f64;
-static ITER_TAKE: usize = 20000;
-static BARRIER: usize = SIZE*3 - 1;
+static ITER_TAKE: usize = 5000;
+const NTHREADS: usize = 12;
 static LOCATIONS: &'static [(usize, usize)] = 
 &[(0, 0), 
   (0, 1), 
@@ -25,15 +25,23 @@ static LOCATIONS: &'static [(usize, usize)] =
   (2, 0), 
   (2, 1),
   (2, 2)];
+static DIRECTIONS: &'static [(i64, i64)] = &[
+ (-1, 1), 
+ (1, 1),
+ (1, 0), 
+ (0, 1), 
+ (-1, 0), 
+ (0, -1), 
+ (-1, -1)
+];
 
 fn main(){
-    println!("{}", srgb_to_clinear(171));
     let sim_app = LightSimApp::init(SIZE);
     let options = eframe::NativeOptions::default();
     eframe::run_native("LightSim", options, Box::new(|_cc| Box::new(sim_app)));
 }
 
-fn eucl_dist(a: (usize, usize), b: (usize, usize)) -> f64{
+fn eucl_dist(a: &(usize, usize), b: &(usize, usize)) -> f64{
     let a_ = (a.0 as f64, a.1 as f64);
     let b_ = (b.0 as f64, b.1 as f64);
     let mut dist = (b_.0 - a_.0) * (b_.0 - a_.0) + (b_.1 - a_.1) * (b_.1 - a_.1);
@@ -43,34 +51,30 @@ fn eucl_dist(a: (usize, usize), b: (usize, usize)) -> f64{
 
 fn get_circle_center(x1: &(usize, usize), x2: &(usize, usize), x3: &(usize, usize)) -> (bool, (usize, usize)){
     //x0
-    //FIX USIZES (EVERYWHERE)
-    let mut y1bracket = x2.0 * x2.0 + x2.1 * x2.1 - x3.0 * x3.0 - x3.1 * x3.1;
-    y1bracket *= x1.1;
-    let mut y2bracket = x3.0 * x3.0 + x3.1 * x3.1 - x1.0 * x1.0 - x1.1 * x1.1;
-    y2bracket *= x2.1;
-    let mut y3bracket = x1.0 * x1.0 + x1.1 * x1.1 - x2.0 * x2.0 - x2.1 * x2.1;
-    y3bracket *= x3.1;
+    //count y_bracket once, multiply in up 
+    let mut y1bracket = (x2.0 * x2.0 + x2.1 * x2.1) as i64 - (x3.0 * x3.0) as i64 - (x3.1 * x3.1) as i64;
+    y1bracket *= x1.1 as i64;
+    let mut y2bracket = (x3.0 * x3.0 + x3.1 * x3.1) as i64 - (x1.0 * x1.0) as i64 - (x1.1 * x1.1) as i64;
+    y2bracket *= x2.1 as i64;
+    let mut y3bracket = (x1.0 * x1.0 + x1.1 * x1.1) as i64 - (x2.0 * x2.0) as i64 - (x2.1 * x2.1) as i64;
+    y3bracket *= x3.1 as i64;
     let up = y1bracket + y2bracket + y3bracket;
-    let down = x1.0 * (x2.1 - x3.1) + x2.0*(x3.1 - x1.1) + x3.0*(x1.1 - x2.1);
+    let down = x1.0 as i64 * (x2.1 as i64 - x3.1 as i64) + x2.0 as i64*(x3.1 as i64 - x1.1 as i64) + x3.0 as i64*(x1.1 as i64 - x2.1 as i64);
     let mut x = (up as f64) / (down as f64);
     x *= -0.5;
-    if x < 0.0{
-        return (false, (0, 0));
-    }
     //y0
-    let mut y1bracket = x2.0 * x2.0 + x2.1 * x2.1 - x3.0 * x3.0 - x3.1 * x3.1;
-    y1bracket *= x1.0;
-    let mut y2bracket = x3.0 * x3.0 + x3.1 * x3.1 - x1.0 * x1.0 - x1.1 * x1.1;
-    y2bracket *= x2.0;
-    let mut y3bracket = x1.0 * x1.0 + x1.1 * x1.1 - x2.0 * x2.0 - x2.1 * x2.1;
-    y3bracket *= x3.0;
+    let mut y1bracket = (x2.0 * x2.0 + x2.1 * x2.1) as i64 - (x3.0 * x3.0) as i64 - (x3.1 * x3.1) as i64;
+    y1bracket *= x1.0 as i64;
+    let mut y2bracket = (x3.0 * x3.0 + x3.1 * x3.1) as i64 - (x1.0 * x1.0) as i64 - (x1.1 * x1.1) as i64;
+    y2bracket *= x2.0 as i64;
+    let mut y3bracket = (x1.0 * x1.0 + x1.1 * x1.1) as i64 - (x2.0 * x2.0) as i64 - (x2.1 * x2.1) as i64;
+    y3bracket *= x3.0 as i64;
     let up = y1bracket + y2bracket + y3bracket;
-    let down = x1.0 * (x2.1 - x3.1) + x2.0*(x3.1 - x1.1) + x3.0*(x1.1 - x2.1);
     let mut y = (up as f64) / (down as f64);
     y *= 0.5;
     //check if eligable
     let mut approved = true;
-    if x < 0.0 || x > BARRIER as f64|| y < 0.0 || y > BARRIER as f64{
+    if x <= -0.5 || y <= -0.5 || x >= 599.5 || y >= 599.5{
         approved = false;
     }
     let x_ = x.round() as usize;
@@ -79,21 +83,39 @@ fn get_circle_center(x1: &(usize, usize), x2: &(usize, usize), x3: &(usize, usiz
 }
 
 fn process_patch(cluster: std::collections::HashMap<usize, Vec<(usize, usize)>>) -> std::collections::HashMap<(usize, usize), usize>{
+    let mut children = vec!();
     let mut answers: std::collections::HashMap<(usize, usize), usize> = std::collections::HashMap::new();
+    //mapreduce
     for cl in cluster{
-        let points = cl.1;
-        let it = points.iter().combinations(3).take(ITER_TAKE);
-        it.for_each(|i|{
-            let cur_ans = get_circle_center(i[0], i[1], i[2]);
-            if cur_ans.0 == true{
-                if answers.contains_key(&cur_ans.1){
-                    *answers.get_mut(&cur_ans.1).unwrap() += 1;
+        children.push(std::thread::spawn(move || -> std::collections::HashMap<(usize, usize), usize> {
+            let mut cur_answers: std::collections::HashMap<(usize, usize), usize> = std::collections::HashMap::new();
+            let points = cl.1;
+            let it = points.iter().combinations(3).take(ITER_TAKE);
+            it.for_each(|i|{
+                let cur_ans = get_circle_center(i[0], i[1], i[2]);
+                if cur_ans.0 == true{
+                    if cur_answers.contains_key(&cur_ans.1){
+                        *cur_answers.get_mut(&cur_ans.1).unwrap() += 1;
+                    }
+                    else{
+                        cur_answers.insert(cur_ans.1, 1);
+                    }
                 }
-                else{
-                    answers.insert(cur_ans.1, 1);
-                }
+            });
+            return cur_answers;
+        }));
+    }
+    let final_result = 
+    children.into_iter().map(|c| c.join().unwrap());
+    for a in final_result{
+        for k in a.keys(){
+            if answers.contains_key(k){
+                *answers.get_mut(k).unwrap() += 1;
             }
-        });
+            else{
+                answers.insert(*k, *a.get(k).unwrap());
+            }
+        }
     }
     return answers;
 }
@@ -116,12 +138,12 @@ fn load_im_egui() -> eframe::epaint::ColorImage{
     return img_;
 }
 
-fn solve_eq(center: (usize, usize), edge: (usize, usize), scene_arr: &ndarray::Array2::<f64>) -> (f64, f64){
-    let b2 = /*srgb_to_clinear*/scene_arr[[center.0, center.1]].powf(1.0 / 3.0);
-    let b1 = /*srgb_to_clinear*/scene_arr[[edge.0, edge.1]].powf(1.0 / 3.0);
+fn solve_eq(ls: (usize, usize), center: (usize, usize), edge: (usize, usize), scene_arr: &ndarray::Array2::<f64>) -> (f64, f64){
+    let b2 = scene_arr[[center.0, center.1]].powf(1.0 / 3.0);
+    let b1 = scene_arr[[edge.0, edge.1]].powf(1.0 / 3.0);
     let diff = b2 - b1;
-    let r1 = eucl_dist((0, 0), edge);
-    let r2 = eucl_dist((0, 0), center);
+    let r1 = eucl_dist(&ls, &edge);
+    let r2 = eucl_dist(&ls, &center);
     let up = b2 * b2 * r2 * r2 - b1 * b1 * r1 * r1;
     let down = (b1 * b1 - b2 * b2) as f64;
     let mut h = up.abs() / down.abs();
@@ -129,7 +151,26 @@ fn solve_eq(center: (usize, usize), edge: (usize, usize), scene_arr: &ndarray::A
     return (h, diff.abs());
 }
 
+fn within_bound(loc: (i64, i64)) -> bool{
+    if loc.0 < 0 || loc.1 < 0 || loc.0 > (SIZE*3 - 1) as i64 || loc.1 > (SIZE*3 - 1) as i64{
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
+fn get_patch(j: usize, k: usize) -> (usize, usize){
+    let mut x = k / SIZE;
+    let mut y = j / SIZE;
+    if x > 2{
+        x = 2;
+    }
+    if y > 2{
+        y = 2;
+    }
+    return (x, y);
+}
 
 impl LightSimApp{
     fn init(sz: usize) -> Self{
@@ -154,29 +195,33 @@ impl LightSimApp{
         }
     }
 
-    fn clusterize_patch(&mut self, loc: &(usize, usize)) -> std::collections::HashMap<usize, Vec<(usize, usize)>>{
+    fn clusterize_patch(&mut self, loc: &(usize, usize)) -> (bool, std::collections::HashMap<usize, Vec<(usize, usize)>>){
         let loc_restrictions = (loc.1 * SIZE, loc.0*SIZE);
         let patch = self.scene_arr.slice(ndarray::s![loc_restrictions.0..loc_restrictions.0+SIZE,loc_restrictions.1..loc_restrictions.1+SIZE]);
+        let (min, max) = find_min_max(&patch.to_owned());
         let mut clusters: std::collections::HashMap<usize, Vec<(usize, usize)>> = std::collections::HashMap::new();
+        if (min - max).abs() < 0.1{
+            return (false, clusters);
+        }
         let shape = patch.shape();
         for i in 0..shape[0]{
             for j in 0..shape[1]{
-                let current_brightness = patch[[i, j]].round() as usize;
+                let current_brightness = (patch[[i, j]] * 100000000.0) as usize;
                 if clusters.contains_key(&current_brightness){
                     clusters.get_mut(&current_brightness).unwrap().push((loc.1 * SIZE + i, loc.0 * SIZE + j));
                 }
-                else{
+                else if  clusters.keys().len()< 5 * NTHREADS{
                     clusters.insert(current_brightness, Vec::new());
                 }
             }
         }
-        return clusters;
+        return (true, clusters);
     }
 
     fn update_(&mut self){
         self.light_source.generate_light_matrix();
         self.scene_arr = self.scene.update(&self.light_source);
-        //self.solve_loc();
+        self.solve_loc();
         self.solve_height();
         self.solve_albedo();
         let img_ = load_im_egui();
@@ -187,44 +232,75 @@ impl LightSimApp{
     fn solve_loc(&mut self){
         let mut answers: std::collections::HashMap<(usize, usize), usize> = std::collections::HashMap::new();
         for loc in LOCATIONS{
-            let clusters = self.clusterize_patch(loc);
-            let pts = process_patch(clusters);
-            for pt in pts{
-                if answers.contains_key(&pt.0){
-                    *answers.get_mut(&pt.0).unwrap() += pt.1;
-                }
-                else{
-                    answers.insert(pt.0, pt.1);
+            let (valid, clusters) = self.clusterize_patch(loc);
+            if valid{
+                let pts = process_patch(clusters);
+                for pt in pts{
+                    if answers.contains_key(&pt.0){
+                        *answers.get_mut(&pt.0).unwrap() += pt.1;
+                    }
+                    else{
+                        answers.insert(pt.0, pt.1);
+                    }
                 }
             }
         }
         let max_elem = answers.iter().max_by_key(|entry| entry.1);
+        if !max_elem.is_none(){
+            self.reverse_solution_location = *max_elem.unwrap().0;
+        }
+    }
+
+    fn launch_ray(&mut self, direction: &(i64, i64)) -> (f64, f64){
+        let mut res = (0.0, 0.0); //height, diff
+        let mut curr_pos = (self.reverse_solution_location.0 as i64, self.reverse_solution_location.1 as i64);
+        let mut mov_pos = curr_pos;
+        while within_bound(mov_pos){
+            let new_pos = (mov_pos.0 + direction.0, mov_pos.1 + direction.1);
+            if within_bound(new_pos){
+                if get_patch(new_pos.0 as usize, new_pos.1 as usize) == 
+                get_patch(curr_pos.0 as usize, curr_pos.1 as usize){
+                    let cur_h = solve_eq(self.reverse_solution_location, (curr_pos.0 as usize, curr_pos.1 as usize),
+                                                    (new_pos.0 as usize, new_pos.1 as usize), 
+                                                    &self.scene_arr);
+                    if cur_h.1 > res.1{
+                        res = cur_h;
+                    }
+                }
+                else{
+                    curr_pos = new_pos;
+                }
+                mov_pos = (mov_pos.0 + direction.0, mov_pos.1 + direction.1);
+            }
+            else{
+                break;
+            }
+        }
+        return res;
     }
 
 
     fn solve_height(&mut self){
-        let light_loc = self.light_source.location;
-        if light_loc == (0, 0){
-            let h1 = solve_eq((0, 0), (199, 199), &self.scene_arr);
-            let h2 = solve_eq((200, 200), (399, 399), &self.scene_arr);
-            let h3 = solve_eq((400, 400), (599, 599), &self.scene_arr);
-            let h4 = solve_eq((200, 0), (399, 0), &self.scene_arr);
-            let h5 = solve_eq((400, 0), (599, 0), &self.scene_arr);
-            let h6 = solve_eq((0, 200), (0, 399), &self.scene_arr);
-            let h7 = solve_eq((0, 400), (0, 599), &self.scene_arr);
-            let mut h_vec: Vec<(f64, f64)> = [h1, h2, h3, h4, h5, h6, h7].to_vec();
-            h_vec.sort_by(|a, b| a.1.total_cmp(&b.1));
-            self.reverse_solution_height = h_vec[h_vec.len() - 1].0.round() as u32;
+        let mut h_vec: Vec<(f64, f64)> = vec!();
+        //rewrite launch ray to non-self, parallel this
+        for dir in DIRECTIONS{
+            let h_c = self.launch_ray(dir);
+            h_vec.push(h_c);
         }
+        h_vec.sort_by(|a, b| a.1.total_cmp(&b.1));
+        self.reverse_solution_height = h_vec[h_vec.len() - 1].0.round() as u32;
     }
 
     fn solve_albedo(&mut self){
-
+        //TODO
+        //count diff on border of two squares for each pair of squares, recount from here.
     }
+    
 }
 
 struct LightSource{
     location: (usize, usize),
+    coordinates: (usize, usize),
     height: u32, //in pixels
     is_on: bool,
     size: usize,
@@ -232,27 +308,14 @@ struct LightSource{
 }
 
 
-fn get_actual_location(location: (usize, usize), size: usize) -> (usize, usize){
-    if location == (0, 0){
-        return (0, 0);
-    }
-    else if location == (0, 2){
-        return (size*3-1, 0);
-    }
-    else if location == (2, 0){
-        return (0, size*3-1);
-    }
-    else if location == (2, 2){
-        return (size*3-1, size*3-1);
-    }
-    else{
-        return (location.1 * size + size/2, location.0 * size + size/2);
-    }
+fn get_actual_location(coordinates: (usize, usize), location: (usize, usize), size: usize) -> (usize, usize){
+    return (location.1 * size + coordinates.0, location.0 * size +coordinates.1);
 }
 
-fn get_light(location: (usize, usize), height: u32, size: usize, j: usize, k: usize) -> f64{
-    let actual_location = get_actual_location(location, size);
-    let ground_dist = eucl_dist(actual_location, (j, k));
+
+fn get_light(coordinates: (usize, usize), location: (usize, usize), height: u32, size: usize, j: usize, k: usize) -> f64{
+    let actual_location = get_actual_location(coordinates, location, size);
+    let ground_dist = eucl_dist(&actual_location, &(j, k));
     let tg_a = ground_dist / (height as f64);
     let alpha = tg_a.atan();
     let mut x = k / size;
@@ -275,6 +338,7 @@ impl LightSource{
         let is_on_ = false;
         return LightSource { 
             location: location_,
+            coordinates: (0, 0),
             height: height_, 
             size: SIZE,
             light_matrix: light_matrix_,
@@ -286,7 +350,7 @@ impl LightSource{
         if self.is_on{
             ndarray::Zip::indexed(self.light_matrix.outer_iter_mut()).par_for_each(|j, mut row| {
                 for (k, col) in row.iter_mut().enumerate(){
-                    *col = get_light(self.location, self.height, self.size, j, k);
+                    *col = get_light(self.coordinates, self.location, self.height, self.size, j, k);
                 }
             });
         }
@@ -404,6 +468,7 @@ fn generate_arr_and_img(sz: usize)-> (ndarray::Array2::<f64>, image::GrayImage){
     return (arr, img)
 }
 
+
 impl Scene{
     fn init(sz: usize) -> Self{
         let (arr, img) = generate_arr_and_img(sz);
@@ -437,6 +502,7 @@ impl Scene{
     }
 }
 
+//GUI
 impl eframe::App for LightSimApp{
         fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame){
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
@@ -456,19 +522,22 @@ impl eframe::App for LightSimApp{
                         ui.selectable_value(&mut self.light_source.location, (2, 1), "(2, 1)");
                         ui.selectable_value(&mut self.light_source.location, (2, 2), "(2, 2)");
                     });
+                    ui.add(eframe::egui::Slider::new(&mut self.light_source.coordinates.0, 0..=SIZE-1).text("Light source X coordinate"));
+                    ui.add(eframe::egui::Slider::new(&mut self.light_source.coordinates.1, 0..=SIZE-1).text("Light source Y coordinate"));
                     ui.add(eframe::egui::Checkbox::new(&mut self.light_source.is_on, "Turn the light on"));
                 });
             self.img_gui.show(ui);
-            if self.light_source.height != 0{
-                self.update_();
-            }
             if ui.button("Save pic").clicked(){
                 let path = "scene_h".to_string() + self.light_source.height.to_string().as_str() + ".png";
                 self.scene.scene_image.save(path).unwrap();
             }
             if self.light_source.is_on{
+                self.update_();
                 ui.label("Reverse task soltions:");
-                ui.label(format!("location: {:?}", self.reverse_solution_location));
+                ui.horizontal(|ui| {
+                    ui.label(format!("location: {:?}", self.reverse_solution_location));
+                    ui.label(format!(" ~ error{}", (eucl_dist(&get_actual_location(self.light_source.coordinates, self.light_source.location, SIZE), &self.reverse_solution_location) as f64 / DIAG.sqrt())));
+                 });
                 ui.horizontal(|ui| {
                     ui.label(format!("height: {}", (self.reverse_solution_height as f32 / DIAG.sqrt() as f32)));
                     ui.label(format!(" ~ error {}", (self.reverse_solution_height.abs_diff(self.light_source.height) as f32/ self.light_source.height as f32)));
